89ad3befea5d1ce07ed11da029ed13382f4057c8
ishaanpgujaran <ishaangujaran@gmail.com>
2026-01-13 01:00:03 +0530

    All Solutions of Practice Problem 1 - LeetCode #1480 : Running Sum of 1D Array

--- 1. Language Basics/1.1 Java Basics/i. Running Sum of 1D Array/RunningSum1D.java
+++ 1. Language Basics/1.1 Java Basics/i. Running Sum of 1D Array/RunningSum1D.java
@@ -2,17 +2,40 @@ import java.util.Scanner;
 
 class RunningSum1D {
     
-    public int[] runningSum(int[] nums) {
+    // My Approach - Not Optimized
+    public int[] runningSum1(int[] nums) {
         int size = nums.length;
         int result[] = new int[size];
 
         for (int i = 0; i < size; i++) {
             for (int j = i; j >= 0; j--) {
-                result[j] += nums[j];
+                result[i] += nums[j];
             }
         }
         
         return result;
+    }
+
+    // Vaaniii Approach - Optimal Solution
+    public int[] runningSum2(int[] nums) {
+        int sum = 0;
+        int result[] = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            result[i] = sum;
+        }
+        
+        return result;
+    }
+
+    // GPT Approach - More Optimized Solution
+    public int[] runningSum3(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        
+        return nums;
     }
 
     public static void main (String[] args) {
@@ -28,11 +51,42 @@ class RunningSum1D {
 
         RunningSum1D obj = new RunningSum1D();
 
-        int result[] = obj.runningSum(nums);
-
+        int result1[] = obj.runningSum1(nums);
+        System.out.println("**My Appraoch**");
         for (int i = 0; i < size; i++) {
-            System.out.print(result[i]+" ");
+            System.out.print(result1[i]+" ");
         }
+        System.out.println("");
+        System.out.println("Time Complexity = O(n^2)");
+        System.out.println("Space Complexity = O(n)");
+        System.out.println("Not Optimized - Works but Slower for Large Inputs");
+        
+        System.out.println("______________________________________________________");
+        System.out.println("");
+
+        int result2[] = obj.runningSum2(nums);
+        System.out.println("**Vaaniii Appraoch**");
+        for (int i = 0; i < size; i++) {
+            System.out.print(result2[i]+" ");
+        }
+        System.out.println("");
+        System.out.println("Time Complexity = O(n)");
+        System.out.println("Space Complexity = O(n)");
+        System.out.println("The Optimal Solution");
+        
+        System.out.println("______________________________________________________");
+        System.out.println("");
+    
+        int result3[] = obj.runningSum3(nums);
+        System.out.println("**GPT Appraoch**");
+        for (int i = 0; i < size; i++) {
+            System.out.print(result3[i]+" ");
+        }
+        System.out.println("");
+        System.out.println("Time Complexity = O(n)");
+        System.out.println("Space Complexity = O(1)");
+        System.out.println("Optimizes Memory More - Modifies the Same Array");
+        System.out.println("#Note - Starting the Loop with i=1 to avoid ArrayIndexOutOfBoundError");
 
         sc.close();
       
