b1470bdb4de165e0377c507b091e005e0f786e3c
ishaanpgujaran <ishaangujaran@gmail.com>
2026-01-28 23:43:14 +0530

    Practice Problem 5  - LeetCode #2011 : Find Value of Variable after Performing Operation

---
+++ 1. Language Basics/1.1 Java Basics/p. Find Value of Variable after Performing Operations /ValueAfterStringOperation.java
@@ -0,0 +1,14 @@
+Import java.util.Scanner;
+
+class ValueAfterStringOperation {
+
+    public int finalValueAfterOperations(String[] operations) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String operations[] = new String[]
+    }
+}
